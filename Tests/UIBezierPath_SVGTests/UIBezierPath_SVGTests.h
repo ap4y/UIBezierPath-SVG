6597c5003405a6bb29b58f307f6e69dfae2b6a15
ap4y <lod@pisem.net>
2013-01-03 14:16:22 +1300

    unit test project added

---
+++ Tests/UIBezierPath_SVGTests/UIBezierPath_SVGTests.h
@@ -0,0 +1,13 @@
+//
+//  UIBezierPath_SVGTests.h
+//  UIBezierPath_SVGTests
+//
+//  Created by ap4y on 1/3/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UIBezierPath_SVGTests : SenTestCase
+
+@end
---
+++ Tests/UIBezierPath_SVGTests/UIBezierPath_SVGTests.m
@@ -0,0 +1,26 @@
+//
+//  UIBezierPath_SVGTests.m
+//  UIBezierPath_SVGTests
+//
+//  Created by ap4y on 1/3/13.
+//
+//
+
+#import "UIBezierPath_SVGTests.h"
+#import "UIBezierPath+SVG.h"
+
+@implementation UIBezierPath_SVGTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    STFail(@"Unit tests are not implemented yet in UIBezierPath_SVGTests");
+}
+
+@end
